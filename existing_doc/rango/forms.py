9b8f7a1a62553ba415d40b857f502845e5a15106
gzpgg3x <gzpgg3x@yahoo.com>
2014-09-28 07:52:09 -0400

    092820140751a
    demoWithSearch

--- existing_doc/rango/forms.py
+++ existing_doc/rango/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the Legal Doc name:")
+    name = forms.CharField(max_length=128, help_text="Please enter the Online Class name:")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
